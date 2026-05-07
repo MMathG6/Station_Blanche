d6fbf11d42f0ebf31838a6ce5ee75914cfdf654b
MMathG6 <godard.m@stjolorient.fr>
2026-05-07 13:47:22 +0200

    Ajout de la connexion entre Flask et MariaDB

--- API_BADGES/api_badge.py
+++ API_BADGES/api_badge.py
@@ -1,12 +1,35 @@
 from flask import Flask, jsonify
+import mysql.connector
 
 app = Flask(__name__)
+
+db_config = {
+    "host": "localhost",
+    "user": "root",
+    "password": "252106",
+    "database": "station_blanche"
+}
 
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({
         "status": "API ONLINE"
     })
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    conn = mysql.connector.connect(**db_config)
+
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM users")
+
+    users = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return jsonify(users)
 
 if __name__ == "__main__":
     app.run(
